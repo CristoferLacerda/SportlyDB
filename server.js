ea595c602691cb408178d7989a8838a7f49be610
CristoferLacerda <cristofersantanalacerda12345@gmail.com>
2025-08-18 15:04:41 -0300

    api

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const mysql = require('mysql2/promise');
-const cors = require('cors');
+// const express = require('express');
 
+import express from 'express'
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(express.json())
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const user = []
 
-// Rota exemplo: listar cursos
-app.get('/api/cursos', async (req, res) => {
-  try {
-    const [rows] = await pool.query('SELECT * FROM curso');
-    res.json(rows);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+app.post('/usuarios', (req, res) => {
+  console.log(req.body);
+
+  res.send('Ok post')
+  
+}); 
+
+app.get('/usuarios', (req, res) => {
+  res.send('Deu certo !')
 });
 
 const PORT = process.env.PORT || 3000;
